c784c8d78d026a2314ccdff7e2090762a20c9071
niicodeer <nicoradin.job@gmail.com>
2022-07-07 11:48:58 -0300

    edit date

--- ejercicio2.js
+++ ejercicio2.js
@@ -2,7 +2,7 @@ const fecha = new Date("july 4 1996")
 const libro = {
     titulo: "Padre Rico, Padre Pobre",
     autor: "Robert Kiyosaki",
-    fecha:"2004",
+    fecha:new Date(2004, 2, 1),
     url:"https://www.unpa.edu.mx/~blopez/algunosLibros/Padre-Rico-Padre-Pobre.pdf",
 }
 const datos = ["Nico", 26, true, fecha, libro]
