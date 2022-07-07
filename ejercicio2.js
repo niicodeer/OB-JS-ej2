0a10524a7bb31e63915c9fc85ac8f98c320b757b
niicodeer <nicoradin.job@gmail.com>
2022-07-07 11:45:03 -0300

    commit

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+const fecha = new Date("july 4 1996")
+const libro = {
+    titulo: "Padre Rico, Padre Pobre",
+    autor: "Robert Kiyosaki",
+    fecha:"2004",
+    url:"https://www.unpa.edu.mx/~blopez/algunosLibros/Padre-Rico-Padre-Pobre.pdf",
+}
+const datos = ["Nico", 26, true, fecha, libro]
+console.log(datos);
